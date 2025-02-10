f2df0a760a78d4be94f52bf58a1817e7a68172eb
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2025-02-10 22:24:29 +0100

    improve layout text homepage

--- src/HomeSections/HomePage.jsx
+++ src/HomeSections/HomePage.jsx
@@ -37,31 +37,31 @@ const HomePage = () => {
         </div>
 
         {/* inset-0: place le div tout en haut-à gauche du container / bg-black/10: /10 = 10% opacity = on ne voit que 10% du bg-noir qui devient transparent pour assombrir l'image et mieux faire apparaitre le texte | /100 = bg tout noir = bg-black */}
-        <div className="absolute inset-0 flex flex-col items-center justify-center pr-96 text-center text-white max-lg:w-80 max-lg:pr-0">
+        <div className=" absolute inset-0 flex flex-col items-center justify-center pr-96 text-center text-white max-lg:w-80 max-lg:pr-20">
           <h1
             data-aos="fade-right"
-            className="font-roboto-bold mt-[-140px] text-[45px] font-bold max-lg:mt-[-40px] max-lg:text-[37px]"
+            className="font-roboto-bold mt-[-140px] text-[45px] font-bold max-lg:mt-[-25px] max-lg:text-[37px]"
           >
             Portfolio
           </h1>
           <h2
             data-aos="fade-right"
             data-aos-delay="500"
-            className="font-roboto-bold mt-10 text-[35px] text-[#0F4C81] max-lg:text-[28px]"
+            className="font-roboto-bold pt-10 max-lg:pt-5 text-[35px] text-[#0F4C81] max-lg:text-[28px]"
           >
             Ines <span className="max-lg:block">Benbrahim Andaloussi</span>
           </h2>
           <h3
             data-aos="fade-right"
             data-aos-delay="1000"
-            className="font-roboto-bold mt-10 text-[30px] max-lg:text-2xl"
+            className="font-roboto-bold pt-10 max-lg:pt-5 text-[30px] max-lg:text-2xl"
           >
             Architecte diplomée d'État
           </h3>
           <h3
             data-aos="fade-right"
             data-aos-delay="1500"
-            className="font-roboto-bold mt-2 text-[30px] max-lg:text-2xl lg:mt-4"
+            className="font-roboto-bold pt-1 max-lg:pt-2 text-[30px] max-lg:text-2xl lg:mt-4"
           >
             ENSA Versailles
           </h3>
