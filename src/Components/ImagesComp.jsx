ff8fa5aeee74c4f7abad7cc06987b37426cd4dd1
Jaafar Bendahou <jaafar.bendahou@gmail.com>
2025-03-26 03:07:33 +0000

    ImagesComp et SaunaFinlance

--- src/Components/ImagesComp.jsx
+++ src/Components/ImagesComp.jsx
@@ -1,9 +1,166 @@
-import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 
-const ImagesComp = () => {
+function Arrow(props) {
+  const { className, style, onClick } = props;
   return (
-    <div>ImagesComp</div>
-  )
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "black",
+        borderRadius: "100%",
+        cursor: "pointer",
+        paddingTop: "0.5px",
+      }}
+      onClick={onClick}
+    />
+  );
 }
 
-export default ImagesComp
+const ImagesComp = ({
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  alt1,
+  alt2,
+  alt3,
+  alt4,
+  alt5,
+  alt6,
+  alt7,
+  alt8,
+}) => {
+  const settings = {
+    nextArrow: <Arrow />,
+    prevArrow: <Arrow />,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptativeHeight: true,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    centerMode: true,
+    centerPadding: "60px",
+    draggable: true,
+    pauseOnFocus: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1, //ça marche comme ça, si on met plus que 1 ça fait une erreur
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <>
+      <div className="mb-36 mt-10 rounded-2xl bg-[#dcdee7]">
+        <Slider
+          {...settings}
+          className="mx-auto max-lg:mt-7 lg:h-[700px] lg:w-[900px]"
+        >
+          <div className="px-2 lg:h-[700px]">
+            <img
+              src={image1}
+              alt={alt1}
+              className="h-full w-full rounded-2xl border-4 border-black object-contain"
+            />
+          </div>
+
+          <div className="px-2 lg:h-[700px]">
+            <img
+              src={image2}
+              alt={alt2}
+              className="h-full w-full rounded-2xl border-4 border-black object-contain"
+            />
+          </div>
+
+          <div className="px-2 lg:h-[700px]">
+            <img
+              src={image3}
+              alt={alt3}
+              className="h-full w-full rounded-2xl border-4 border-black object-contain"
+            />
+          </div>
+
+          <div className="px-2 lg:h-[700px]">
+            <img
+              src={image4}
+              alt={alt4}
+              className="h-full w-full rounded-2xl border-4 border-black object-contain"
+            />
+          </div>
+
+          <div className="px-2 lg:h-[700px]">
+            <img
+              src={image5}
+              alt={alt5}
+              className="h-full w-full rounded-2xl border-4 border-black object-contain"
+            />
+          </div>
+
+          {/* Il y a forcement 5 images, mais pas + en fonction du projet: donc je dis que si image6 existe alors montre une image avec un cadre, pour que s'il n'y a rien, on ne voit pas un cadre vide */}
+          {image6 && (
+            <div className="px-2 lg:h-[700px]">
+              <img
+                src={image6}
+                alt={alt6}
+                className="h-full w-full rounded-2xl border-4 border-black object-fill"
+              />
+            </div>
+          )}
+
+          {image7 && (
+            <div className="px-2 lg:h-[700px]">
+              <img
+                src={image7}
+                alt={alt7}
+                className="h-full w-full rounded-2xl border-4 border-black object-fill"
+              />
+            </div>
+          )}
+
+          {image8 && (
+            <div className="px-2 lg:h-[700px]">
+              <img
+                src={image8}
+                alt={alt8}
+                className="h-full w-full rounded-2xl border-4 border-black object-fill"
+              />
+            </div>
+          )}
+        </Slider>
+      </div>
+    </>
+  );
+};
+
+export default ImagesComp;
--- src/Pages/SaunaFinlande.jsx
+++ src/Pages/SaunaFinlande.jsx
@@ -7,6 +7,7 @@ import {
 } from "../assets/images/projetsEcole/saunaFin";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
+import ImagesComp from "../Components/ImagesComp";
 
 const SaunaFinlande = () => {
   return (
@@ -21,49 +22,46 @@ const SaunaFinlande = () => {
         </p>
       </div>
 
-      <div className="mx-auto h-[700px] w-[1000px] shadow-xl">
-        <img
-          src={projet1}
-          alt="Sauna en Finlande"
-          className="h-full w-full object-cover object-bottom"
-        />
-      </div>
+      <ImagesComp
+        image1={projet1}
+        alt1="Sauna en Finlande 1"
+        image2={saunaFin1}
+        alt2="Sauna en Finlande 2"
+        image3={saunaFin2}
+        alt3="Sauna en Finlande 3"
+        image4={saunaFin3}
+        alt4="Sauna en Finlande 4"
+        image5={saunaFin4}
+        alt5="Sauna en Finlande 5"
+      />
 
-      <div className="grid grid-cols-2">
-        <div>
-          <img src={saunaFin1} alt="sauna Finlande 1" />
-          <img src={saunaFin2} alt="sauna Finlande 2" />
-          <img src={saunaFin3} alt="sauna Finlande 3" />
-          <img src={saunaFin4} alt="sauna Finlande 4" />
-        </div>
-        <div>
-          <p>
-            Le sauna s'insère sur le site de la maison expérimentale d'Alvar
-            Aalto à Muuratsalo, près du lac pour permettre les aller-retour vers
-            l'eau froide.
-          </p>
-          <p>
-            {" "}
-            Si l'on ne retrouve traditionnellement la pierre que sous la forme
-            d'un amas au dessus du feu, dans un sauna, ses qualités physiques et
-            visuelles savent procurer des sensations fortes que ce projet, donne
-            à voir et à ressentir.
-          </p>
-          <p>
-            La pierre expose ses qualités et donne accès à ses sensations.
-            <br />
-            Le principe du sauna est celui d'une masse de pierres qui prends une
-            forme propre; on passe d'une pierre sauvage à une architecture de
-            pierre plus maîtrisée jusqu'aux pierres du sauna.
-            <br />
-            Les pierres ici sont également une ressource de chaleur.
-          </p>
-          <p>
-            Alors qu'en Finlande les saunas sont la plupart du temps en bois,
-            celui-ci redonne la valeur à la pierre qui est l'initiatrice du
-            sauna Finlandais, sans lequelle il n'existe pas.
-          </p>
-        </div>
+      <div>
+        <p>
+          Le sauna s'insère sur le site de la maison expérimentale d'Alvar Aalto
+          à Muuratsalo, près du lac pour permettre les aller-retour vers l'eau
+          froide.
+        </p>
+        <p>
+          {" "}
+          Si l'on ne retrouve traditionnellement la pierre que sous la forme
+          d'un amas au dessus du feu, dans un sauna, ses qualités physiques et
+          visuelles savent procurer des sensations fortes que ce projet, donne à
+          voir et à ressentir.
+        </p>
+        <p>
+          La pierre expose ses qualités et donne accès à ses sensations.
+          <br />
+          Le principe du sauna est celui d'une masse de pierres qui prends une
+          forme propre; on passe d'une pierre sauvage à une architecture de
+          pierre plus maîtrisée jusqu'aux pierres du sauna.
+          <br />
+          Les pierres ici sont également une ressource de chaleur.
+        </p>
+        <p>
+          Alors qu'en Finlande les saunas sont la plupart du temps en bois,
+          celui-ci redonne la valeur à la pierre qui est l'initiatrice du sauna
+          Finlandais, sans lequelle il n'existe pas.
+        </p>
       </div>
 
       <Footer />
